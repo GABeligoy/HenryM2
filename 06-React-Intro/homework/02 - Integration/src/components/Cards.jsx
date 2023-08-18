9228f4203621a06c8491caf313276ec196650e03
Gustavo Beligoy <gustavobeligoy@gmail.com>
2023-08-18 17:53:45 -0300

    Integrador

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,5 +1,17 @@
 import Card from './Card';
 
 export default function Cards(props) {
-   return <div></div>;
+   return (<div>
+      
+      {props.characters.map((elem)=>(
+         <Card key={elem.id}
+         name={elem.name}
+         status={elem.status}
+         species={elem.species}
+         gender={elem.gender}
+         origin={elem.origin}
+         image={elem.image}
+         />
+      ))}
+   </div>);
 }
